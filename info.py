397790980e6c1a2b5b8dbcc345f741e34e0222e1
Neeraj12231 <139227759+Neeraj12231@users.noreply.github.com>
2024-03-10 07:32:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,13 +100,13 @@ NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME')
+    APP_NAME = environ.get('Tyun')
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://demo-gsmz.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://demo-gsmz.onrender.com/".format(FQDN, PORT)
+URL = "https://tyun-abf64f1d0799.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://tyun-abf64f1d0799.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -115,15 +115,15 @@ name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME'))
+    APP_NAME = str(getenv('Tyun'))
 
 else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://wild-nadiya-anonameous.koyeb.app/".format(FQDN)
+    URL = "https://tyun-abf64f1d0799.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://wild-nadiya-anonameous.koyeb.app/".format(FQDN)
+    URL = "https://tyun-abf64f1d0799.herokuapp.com/".format(FQDN)
 
 
 
